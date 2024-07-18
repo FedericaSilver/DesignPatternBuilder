899b6130cad857c714acc514229d6b315637f0a3
Federica Parillo <federicaparillo.94@gmail.com>
2024-07-18 15:36:06 +0200

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        PersonBuilder personBuilder = new PersonBuilder();
-        personBuilder.setAge(18);
-        personBuilder.setFirstName("Mol");
-        personBuilder.setLastName("Trev");
-        personBuilder.setAddress("Via della vittoria");
-        Person person3 = personBuilder.build();
-        System.out.println(person3);
-        Person person = new PersonBuilder().build();
-        person.setFirstName("Mario");
-        person.setLastName("Rossi");
-        Person person2 = new PersonBuilder().build();
-        person2.setFirstName("Pietro");
-        person2.setLastName("Verdi");
-        person2.setAge(19);
-        person2.setAddress("Viale Primavera, 9");
+
+        Person person = new Person.PersonBuilder("Mario", "Rossi").build();
+        person.setAge(29);
+        Person person1 = new Person.PersonBuilder("Loris", "verdi").setAge(21).setAddress("Viale Primavera, 6").build();
         System.out.println(person);
-        System.out.println(person2);
+        System.out.println(person1);
+
 
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -51,4 +51,55 @@ public class Person {
                 ", age: " + age +
                 ", address: " + address + '\'';
     }
+
+    public static class PersonBuilder {
+        private String firstName;
+        private String lastName;
+        private int age;
+        private String address;
+
+        public PersonBuilder(String firstName, String lastName){
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public PersonBuilder setFirstName(String fistName) {
+            this.firstName = fistName;
+            return this;
+        }
+
+        public PersonBuilder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public PersonBuilder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public PersonBuilder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+        public Person build(){
+            return new Person(this);
+        }
+    }
 }
